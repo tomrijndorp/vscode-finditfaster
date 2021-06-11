63e53d8899dce5271b8756fdec111cf4792b85a9
Tom Rijndorp <tomrijndorp@gmail.com>
2021-06-11 01:16:17 -0700

    Back to terminal solution. Kinda works a bit

--- src/extension.ts
+++ src/extension.ts
@@ -7,152 +7,77 @@ import * as cp from 'child_process';
 import { cwd } from 'process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+let term: vscode.Terminal;
+// const command = 'fzf --preview "bat --force-colorization --plain {}" | xargs -I{} open "vscode://file/$(pwd)/{}"; clear';
+
+/**
+ * TODO:
+ * [ ] Auto hide terminal when done
+ * [ ] Handle spaces in filenames
+ * [ ] Linux support
+ * [ ] Windows support
+ * [ ] Preferences / options
+ */
+
+/**
+ * Couple of observations:
+ * 
+ * 1. On Mac OS, opening using open with a URI is _way_ faster than using the `code` command.
+ * 2. Depending on the file extension, XCode (?!) will complain that no application is registered,
+ *    _even though_ the URI starts with vscode://.
+ *    Therefore, we'll pass in the application path. Unfortunately, we can't use the `code` command
+ *    for this either, and we'll have to know where VS Code is installed.
+ */
+
+const vsCodePath = '/Applications/Visual Studio Code.app';
+
+const command = `fzf --multi --print0 --preview "bat --force-colorization --plain {}" | xargs -0 -I{} open -a "${vsCodePath}" "vscode://file/$(pwd)/{}"`;
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-ripgrep" is now active!');
-
-    let ls: string[];
-    cp.exec('rg /usr/bin --files', (err, stdout, stderr) => {
-        // console.log('stdout: ' + stdout);
-        // console.log('stderr: ' + stderr);
-        // if (err) {
-        // 	console.log('error: ' + err);
-        // }
-        // const qp = vscode.window.createQuickPick();
-        ls = stdout.split('\n');
-        // qp.onDidHide(() => qp.dispose());
-        // qp.show();
-    });
-
-    // const t = cp.exec('fzf', { cwd: '/Users/tomrijndorp' }, (err, stdout, stderr) => {
-    //     if (err) {
-    //         console.log('*******problem', err);
-    //     }
-    //     console.log('Completed:', stdout, stderr);
-    // });
-
-    // console.log('pid', t.pid);
-    // const term = vscode.window.createTerminal('Naam',);
-    // term.show();
-    // term.sendText('vif');
-    // cp.spawnSync('fzf', [], {stdio: 'inherit'});
-    // vscode.window.ondidwrite
-    // const writeEmitter = new vscode.EventEmitter<string>();
-    // writeEmitter.
-    // const pty: vscode.Pseudoterminal = {
-    // 	onDidWrite: writeEmitter.event,
-    // 	open: () => {},
-    // 	close: () => {},
-    // 	handleInput: data => writeEmitter.fire(data + 'q'),
-    // };
-    // const term = vscode.window.createTerminal({name: 'Yyoyo', pty});
-    // term.show();
-    // term.sendText('cd haha!\n');
-    // wv.webview.postMessage()
-    function showWv() {
-        const wv = vscode.window.createWebviewPanel('bla', 'Titel', vscode.ViewColumn.Active, { enableScripts: true });
-        wv.webview.html = wvContent;
-        // wv.webview.postMessage({
-        //     setContents: ls.join('<br/>'),
-        // });
-        // t.stdout?.on('data', data => { console.log('stdout new data!', data); wv.webview.postMessage({ setContents: data }); });
-        wv.webview.onDidReceiveMessage(m => {
-            console.log(m);
-            const t = cp.exec(`bash -i -c "fd ${m} --max-results 20 -tf"`, { cwd: '/Users/tomrijndorp/Dropbox' }, (err, stdout, stderr) => {
-            // const t = cp.exec(`bash -i -c "fzf"`, { cwd: '/Users/tomrijndorp/Dropbox' }, (err, stdout, stderr) => {
-                if (err) {
-                    console.log('*******problem', err);
-                }
-                console.log('Completed:', t.exitCode + '\n' + stdout, stderr);
-                wv.webview.postMessage({ setContents: stdout });
-            });
-            // t.stdin?.write(m);
-        });
-    }
-
+    prepareTerminal();
     let d2 = vscode.commands.registerCommand('vscode-ripgrep.shellThing', () => {
         // vscode.window.showInformationMessage('jaja');
-        showWv();
+        // showWv();
+        showNext();
     });
-
-    vscode.commands.executeCommand('vscode-ripgrep.shellThing');
-
-    // const ib = vscode.window.showInputBox({
-    // 	title: 'Titel\nlijn 2\n\nlijn vier',
-    // 	prompt: 'Zeg het eens?',
-    // 	value: 'De waarde',
-    // });
-    // const ib = vscode.window.createInputBox();
-    // ib.placeholder = 'Place hodler';
-    // ib.show();
-    // vscode.window.sho
-
-
-    // 1. Trigger command
-    // 3. Open some UI
-    // 2. Start a process with fzf
-    //    - Get FZF to read from e.g. a socket that we write data to interactively as we post stuff in here
-
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    // let disposable = vscode.commands.registerCommand('vscode-ripgrep.helloWorld', () => {
-    // 	// The code you place here will be executed every time your command is executed
-    // 	// Display a message box to the user
-    // 	vscode.window.showInformationMessage('Hello World from vscode-ripgrep.');
-    // });
-
-    // context.subscriptions.push(disposable);
-    // context.subscriptions.push(d2);
-
 }
-
-
-const wvContent = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Wut?</title>
-</head>
-<body>
-    <input type='text' id='query' style='width: 500px' placeholder='Type here...' onKeyUp='notify(this.value);' />
-    <pre id="mine">0</pre>
-
-    <script>
-        const vscode = acquireVsCodeApi();
-
-        function notify(e) {
-            console.log(e);
-            vscode.postMessage(e);
-        }
-
-        (function() {
-
-	    // Handle the message inside the webview
-	    window.addEventListener('message', event => {
-    
-		const message = event.data; // The JSON data our extension sent
-		console.log('RX:', message);
-		const el = document.getElementById('mine');
-		// el.textContent = JSON.stringify(message);
-		if (message.setContents !== undefined) {
-			el.innerHTML = message.setContents;
-            if (message.setContents === '') {
-                console.log('zilch');
-                el.innerHTML = '&lt;empty response&gt;';
-            }
-		}
-
-        document.getElementById('query').focus();
-
-	    });
-        }())
-    </script>
-</body>
-</html>`;
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+function prepareTerminal() {
+    // TODO lazy instantiation in case terminal is closed (first use / user closed terminal)
+    vscode.window.showInformationMessage('prep');
+    term = vscode.window.createTerminal({
+        name: 'RIPPIT!',
+        cwd: '/Users/tomrijndorp/Dropbox',
+        hideFromUser: true,
+        env: { },
+    });
+}
+
+function showNext() {
+    if (term.exitStatus !== undefined) {
+        prepareTerminal();
+    }
+    term.sendText(command);
+    term.show();
+    vscode.commands.executeCommand('workbench.action.toggleMaximizedPanel');
+}
+
+
+
+
+
+// function stuff() {
+//     // Use the console to output diagnostic information (console.log) and errors (console.error)
+//     // This line of code will only be executed once when your extension is activated
+//     console.log('Congratulations, your extension "vscode-ripgrep" is now active!');
+
+//     // console.log('pid', t.pid);
+
+
+// }
