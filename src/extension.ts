409c6fbe09ac1069f9e06042d2626e77f704b1fe
Tom Rijndorp <tomrijndorp@gmail.com>
2021-06-11 23:37:06 -0700

    Really getting somewhere
    I do think I just broke some stuff... :( Happened as I was introducing
    support for multiple workspace directories. Also changed from fd to
    rg.

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,17 @@ import * as vscode from 'vscode';
 import { workspace } from 'vscode';
 
 import * as cp from 'child_process';
-import { cwd } from 'process';
+import { cwd, uptime } from 'process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+import { watch } from 'fs';
 
 let term: vscode.Terminal;
 // const command = 'fzf --preview "bat --force-colorization --plain {}" | xargs -I{} open "vscode://file/$(pwd)/{}"; clear';
 
 /**
  * TODO:
- * [ ] Auto hide terminal when done
+ * [x] Auto hide terminal when done
  * [ ] Handle spaces in filenames
  * [ ] Linux support
  * [ ] Windows support
@@ -30,17 +31,81 @@ let term: vscode.Terminal;
  *    for this either, and we'll have to know where VS Code is installed.
  */
 
-const vsCodePath = '/Applications/Visual Studio Code.app';
+const CFG: {
+    folders: string[],
+    vsCodePath: string,
+    workspaceSettings: {
+        folders: string[],
+    },
+    canaryFile: string | null,
+    terminalWasVisibleBeforeCommand: boolean | null,
+    lastActiveTerminal: vscode.Terminal | undefined,
+} = {
+    folders: [],
+    vsCodePath: '/Applications/Visual Studio Code.app',
+    workspaceSettings: {
+        folders: [],
+    },
+    canaryFile: null,
+    terminalWasVisibleBeforeCommand: null,
+    lastActiveTerminal: undefined,
+};
 
-const command = `fzf --multi --print0 --preview "bat --force-colorization --plain {}" | xargs -0 -I{} open -a "${vsCodePath}" "vscode://file/$(pwd)/{}"`;
+const getCommand = () => {
+    const paths = CFG.folders.join(' ');
+    const cmd = `
+    rg --files --hidden ${paths} 2>/dev/null | \
+    fzf \
+        --multi \
+        --print0 \
+        --preview "bat --force-colorization --plain {}" | \
+    tee /tmp/lastOutput |
+    xargs \
+        -0 -I{} \
+        open -a "${CFG.vsCodePath}" "vscode://file/{}"; \
+    echo $? > ${CFG.canaryFile}; \
+    clear
+    `;
+    console.log(cmd);
+    return cmd;
+};
+
+function handleWorkspaceFoldersChanges() {
+    const updateFolders = () => {
+        const dirs = vscode.workspace.workspaceFolders;
+        if (dirs === undefined) {
+            CFG.folders = ['.'];   // best we can do
+        } else {
+            CFG.folders = dirs.map(x => {
+                const uri = x.uri.toString();
+                if (uri.substr(0, 7) === 'file://') {
+                    return uri.substr(7);
+                } else {
+                    vscode.window.showErrorMessage('Non-file:// uri\'s not currently supported...');
+                    return '';
+                }
+            });
+            console.log('workspace folders:', CFG.folders);
+        }
+    };
+
+    updateFolders();
+
+    // Also re-update when anything changes
+    vscode.workspace.onDidChangeWorkspaceFolders(event => {
+        console.log('workspace folders changed: ', event);
+        updateFolders();
+    });
+}
 
 
 export function activate(context: vscode.ExtensionContext) {
-
-    prepareTerminal();
-    let d2 = vscode.commands.registerCommand('vscode-ripgrep.shellThing', () => {
-        // vscode.window.showInformationMessage('jaja');
-        // showWv();
+    // Because we can't determine what was going on in the terminal panel before,
+    // let's just make it a setting for now.
+    CFG.terminalWasVisibleBeforeCommand = false;  // so now we'll always close it
+    handleWorkspaceFoldersChanges();
+    reinitialize();
+    vscode.commands.registerCommand('vscode-ripgrep.shellThing', () => {
         showNext();
     });
 }
@@ -48,14 +113,56 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
+function reinitialize() {
+
+    // CFG.folders = vscode.workspace.getConfiguration().get<any[]>('folders')?.map(x => x.path) || [];
+    // console.log(`workspace folders: ${CFG.folders}`);
+    // const x = vscode.workspace.getConfiguration().get('folders');
+    // console.log('folders:', x);
+    // TODO figure this out
+    //
+    // Set up a file watcher. Any time there is output to our "canary file", we hide the terminal (because the command was completed)
+    //
+    let watcher;
+    cp.exec('mktemp', (err, stdout, stderr) => {
+        if (err) {
+            vscode.window.showErrorMessage(`Failed to initialize plugin (failed to create file watcher: "${stdout}${stderr}")`);
+        } else {
+            CFG.canaryFile = stdout.trim();
+            watcher = watch(CFG.canaryFile, (eventType, fileName) => {
+                if (eventType === 'change') {
+                    // Switch back to the terminal the user was on before running our code
+                    // if (CFG.lastActiveTerminal !== term && CFG.lastActiveTerminal !== undefined) {
+                    //     console.log('A different terminal was active before. Focusing back on that one.', term);
+                    //     // CFG.lastActiveTerminal.show();
+                    // }
+                    // CFG.lastActiveTerminal?.show();
+                    if (CFG.terminalWasVisibleBeforeCommand === false) {
+                        // console.log('hiding terminal', term);
+                        // whichever one we just selected
+                        // vscode.window.activeTerminal?.hide();
+                        term.hide();
+                    }
+                }
+            });
+
+            //
+            // Prepare the terminal for first use. We already enter the command so the user doesn't have to wait.
+            //
+            prepareTerminal();
+
+        }
+    });
+
+}
+
 function prepareTerminal() {
     // TODO lazy instantiation in case terminal is closed (first use / user closed terminal)
-    vscode.window.showInformationMessage('prep');
     term = vscode.window.createTerminal({
-        name: 'RIPPIT!',
-        cwd: '/Users/tomrijndorp/Dropbox',
+        name: '⚡️',
+        cwd: '/Users/tomrijndorp',  // TODO pref
         hideFromUser: true,
-        env: { },
+        env: {},
     });
 }
 
@@ -63,21 +170,12 @@ function showNext() {
     if (term.exitStatus !== undefined) {
         prepareTerminal();
     }
-    term.sendText(command);
+    term.sendText(getCommand());
+    // vscode.commands.executeCommand('workbench.action.toggleMaximizedPanel');
+    // We can't, with vscode's API, I think, determine whether the terminal panel was open or
+    // not, or what it was showing before we took over. This is unfortunate, not sure how to
+    // fix it.
+    // CFG.terminalWasVisibleBeforeCommand = 
+    CFG.lastActiveTerminal = vscode.window.activeTerminal;
     term.show();
-    vscode.commands.executeCommand('workbench.action.toggleMaximizedPanel');
 }
-
-
-
-
-
-// function stuff() {
-//     // Use the console to output diagnostic information (console.log) and errors (console.error)
-//     // This line of code will only be executed once when your extension is activated
-//     console.log('Congratulations, your extension "vscode-ripgrep" is now active!');
-
-//     // console.log('pid', t.pid);
-
-
-// }
