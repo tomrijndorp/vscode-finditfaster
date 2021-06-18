d96714fb8c5c5cdabc1937138b643778d253284f
Tom Rijndorp <tomrijndorp@gmail.com>
2021-06-17 21:56:10 -0700

    I think we have the second script largely working
    Still some path issues

--- find_it_faster.sh
+++ find_it_faster.sh
@@ -22,11 +22,10 @@ if [[ -z "$VAL" ]]; then
     echo canceled
     echo "1" > "$CANARY_FILE"
     exit 1
+else
+    echo "$VAL" | while read -r LINE; do
+        URINAME=${LINE// /%20}
+        open -a "$VSCODE_PATH" "vscode://file/$URINAME"
+    done
+    echo "0" > "$CANARY_FILE"
 fi
-
-echo "$VAL" | while read -r LINE; do
-    URINAME=${LINE// /%20}
-    open -a "$VSCODE_PATH" "vscode://file/$URINAME"
-done
-
-echo "0" > "$CANARY_FILE"
--- find_it_faster_within.sh
+++ find_it_faster_within.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # 1. Search for text in files using Ripgrep
 # 2. Interactively restart Ripgrep with reload action
 # 3. Open the file in Vim
-RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case '.*' $@"
-IFS=: read -ra selected < <(
-  FZF_DEFAULT_COMMAND="$RG_PREFIX" \
+RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+# We match against the beginning of the line so everything matches but nothing gets highlighted
+INITIAL_REGEX="^"
+# IFS sets the delimiter
+# -r: raw
+# -a: array
+PATHS=("$@")
+PATHS_STR="${PATHS[*]}"
+
+IFS=: read -ra VAL < <(
+  FZF_DEFAULT_COMMAND="$RG_PREFIX $INITIAL_REGEX $PATHS_STR" \
   fzf --ansi \
-      --disabled --query "$INITIAL_QUERY" \
-      --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+      --disabled --query "" \
+      --bind "change:reload:sleep 0.1; $RG_PREFIX {q} $PATHS_STR || true" \
       --delimiter : \
       --preview 'bat --color=always {1} --highlight-line {2}' \
       --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
 )
-[ -n "${selected[0]}" ] && vim "${selected[0]}" "+${selected[1]}"
+# Output is filename, line number, character, contents
+
+# echo "We got ${VAL[*]}"
+if [[ ${#VAL[@]} -eq 0 ]]; then
+    echo canceled
+    echo "1" > "$CANARY_FILE"
+    exit 1
+else
+    URINAME=${VAL[0]// /%20}:${VAL[1]}
+    open -a "$VSCODE_PATH" "vscode://file/$URINAME"
+    echo "0" > "$CANARY_FILE"
+fi
