129eacb8fc781908364f127131301c060fcc3933
Ian Davids <ihdavids@gmail.com>
2022-04-23 21:55:24 -0700

    Windows support for search locations

---
+++ list_search_locations.ps1
@@ -0,0 +1,16 @@
+
+$PATHS=$args
+
+clear
+Write-Host "----------"
+Write-Host "The following files are on your search path:" -ForegroundColor Cyan
+foreach ($P in $PATHS) {
+    Write-Host "- $P"
+}
+$fc=Get-Content "$Env:EXPLAIN_FILE" -Raw
+Write-Host ""
+Write-Host "$fc"
+Write-Host "----------"
+Write-Host ""
+Write-Host "All these paths are configurable. If you\'re expecting a path but not seeing it here,"
+Write-Host "it may be turned off in the settings.\n\n"
